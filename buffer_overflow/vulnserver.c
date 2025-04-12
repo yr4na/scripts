4277924f202d993d40bfd023f111aae018442f83
Yarana <yaranawr@gmail.com>
2025-04-12 02:14:59 -0300

    Add: purposefully designed vulnerable server intended for buffer overflow studies.

---
+++ buffer_overflow/vulnserver.c
@@ -0,0 +1,126 @@
+/**
+ * Purposefully designed vulnerable server intended for buffer overflow studies.
+ */
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define DEFAULT_PORT "9999"
+
+void received_name(const char *name) {
+	printf("Name received: %s\n", name);
+}
+
+void handle_client(SOCKET client_sock) {
+	char buffer[500];
+	char name[500];
+	
+	const char *msg = "Enter your name: ";
+	send(client_sock, msg, strlen(msg), 0);
+	
+	int received = recv(client_sock, buffer, 1000, 0);
+	if  (received <= 0) {
+		closesocket(client_sock);
+		return;
+	}
+	
+	buffer[received] = '\0';
+	
+	int i;
+	for (i = 0; i < received; i++) {
+        if (buffer[i] == '\r' || buffer[i] == '\n') {
+            buffer[i] = '\0';
+            break;
+        }
+    }
+	
+	received_name(buffer);
+	
+	char welcome_msg[600];
+    sprintf(welcome_msg, "Welcome, %s!\n", buffer);
+    send(client_sock, welcome_msg, strlen(welcome_msg), 0);
+	
+	closesocket(client_sock);
+}
+
+int main() {
+	WSADATA wsaData;
+	SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+        			*ptr = NULL,
+        			hints;
+    int iResult;
+	
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+    
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE; 
+    
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+    
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket failed: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+    
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    
+    freeaddrinfo(result);
+    
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Server listening on port 9999...\n");
+    
+    while (1) {
+        ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) {
+            printf("accept failed: %d\n", WSAGetLastError());
+            closesocket(ListenSocket);
+            WSACleanup();
+            return 1;
+        }
+        
+        printf("New connection received!\n");
+        
+        handle_client(ClientSocket);
+    }
+
+    closesocket(ListenSocket);
+    WSACleanup();
+
+    return 0;
+    
+}
